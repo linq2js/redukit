fdbf411e6f25ad40c2c6e94c9be2b5947dc02ef2
hung-showwcase <hung@showwcase.com>
2021-09-09 14:50:33 +0700

    feat: useAction

--- utils/createWrapper.d.ts
+++ utils/createWrapper.d.ts
@@ -1,3 +1,3 @@
 import * as React from "react";
-import { AsyncStore } from "../lib";
-export default function createWrapper<TState = any>(...args: any[]): [React.FC, AsyncStore<TState>];
+import { Store } from "redux";
+export default function createWrapper<TState = any>(...args: any[]): [React.FC, Store<TState>];
